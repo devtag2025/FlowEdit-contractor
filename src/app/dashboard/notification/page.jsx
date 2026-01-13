157d8f5f99c03363f35bd9fe2d6fcd2e94cdba64
FaiqaN27 <faiqanadeem6725@gmail.com>
2026-01-13 10:42:00 +0500

    complete notification screen

--- src/app/dashboard/notification/page.jsx
+++ src/app/dashboard/notification/page.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import { Button } from "@/components/common/Button";
+import NotificationBar from "@/components/notification/NotificationBar";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const NotificationPage = () => {
+  const btns = [
+    {
+      label: "All",
+    },
+    {
+      label: "Project Updates",
+    },
+    {
+      label: "System Alerts",
+    },
+    {
+      label: "Billing",
+    },
+  ];
+
+  const notifications = [
+  {
+    id: 1,
+    title: "Pharetra pharetra eget enim massa et",
+    description:
+      "Choose the plan that best fits your content needs. All plans include professional editing and fast delivery.",
+    type: "project",
+    time: "2 hours ago",
+  },
+  {
+    id: 2,
+    title: "Nec nunc aenean orci odio tincidunt odio",
+    description:
+      "Choose the plan that best fits your content needs. All plans include professional editing and fast delivery.",
+    type: "system",
+    time: "2 hours ago",
+  },
+  {
+    id: 3,
+    title: "A bibendum arcu faucibus in. Morbi in",
+    description:
+      "Choose the plan that best fits your content needs. All plans include professional editing and fast delivery.",
+    type: "billing",
+    time: "2 hours ago",
+  },
+];
+
+  const [active, setActive] = useState("All");
+
   return (
-    <div>page</div>
-  )
-}
+    <main className="min-h-screen bg-secondary px-3 md:px-8 py-5 pb-10 ">
+      <h1 className="text-accent font-semibold text-2xl md:text-3xl">
+        Notifications
+      </h1>
 
-export default page
+      {btns.map((btn) => (
+        <Button
+          key={btn.label}
+          onClick={()=> setActive(btn.label)}
+          className={`text-sm px-3 md:px-4 py-1 rounded-lg mr-1 md:mr-3 my-4 md:my-8  ${
+            active === btn.label
+              ? `bg-primary text-white`
+              : `bg-tertiary text-accent`
+          }`}
+        >
+          {btn.label}
+        </Button>
+      ))}
+
+      <section className="flex justify-between items-center border border-slate-400 b-2 rounded-2xl p-6">
+        <div className="flex flex-col">
+          <h3 className="text-accent font-semibold text-xl md:text-2xl mb-2">Important Updates</h3>
+          <p className="text-accent text-sm md:text-lg">
+            Choose the plan that best fits your content needs.All plans include
+            professional editing and fast delivery.
+          </p>
+        </div>
+
+        <Link href="/dashboard/service">
+          <ChevronRight className="flex items-center text-accent" size={40} />
+        </Link>
+      </section>
+
+      <NotificationBar notifications={notifications} />
+    </main>
+  );
+};
+
+export default NotificationPage;
---
+++ src/components/notification/NotificationBar.jsx
@@ -0,0 +1,53 @@
+import { MessageCircle } from "lucide-react";
+import React from "react";
+
+const NotificationBar = ({ notifications }) => {
+  return (
+    <div className="space-y-4 mt-4">
+      {/* Desktop */}
+      {notifications.map((notification) => (
+        <div
+          key={notification.id}
+          className="hidden bg-tertiary rounded-2xl p-5 md:flex justify-between"
+        >
+          <div className="flex items-center justify-center gap-4">
+            <div className="bg-primary p-3 rounded-full">
+              <MessageCircle size={18} />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <h4 className="text-accent font-semibold text-lg mb-1">
+                {notification.title}
+              </h4>
+              <p className="text-slate-600 text-sm">
+                {notification.description}
+              </p>
+            </div>
+          </div>
+
+          <span className="text-sm text-slate-600 whitespace-nowrap">
+            {notification.time}
+          </span>
+        </div>
+      ))}
+
+      {/* Mobile */}
+      {notifications.map((notification) => (
+        <div
+          key={notification.id}
+          className="md:hidden bg-tertiary rounded-2xl p-5 flex flex-col"
+        >
+          <span className="text-sm text-slate-600 whitespace-nowrap">
+            {notification.time}
+          </span>
+          <h4 className="text-accent font-semibold text-lg mb-1">
+            {notification.title}
+          </h4>
+          <p className="text-slate-600 text-sm">{notification.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NotificationBar;
