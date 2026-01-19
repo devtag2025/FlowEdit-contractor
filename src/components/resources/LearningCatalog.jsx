17047c50bc1aa7c7e2f02840e7627fdac30f0c6a
FaiqaN27 <faiqanadeem6725@gmail.com>
2026-01-19 15:09:00 +0500

    feat: Resource Page Ui Completed

---
+++ src/components/resources/LearningCatalog.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LearningCatalog = () => {
+  return <div>LearningCatalog</div>;
+};
+
+export default LearningCatalog;
---
+++ src/utils/resource.js
@@ -0,0 +1,40 @@
+export const onboardingSteps = [
+  { id: 1, label: "Start", icon: "play" },
+  { id: 2, label: "Account", icon: "user" },
+  { id: 3, label: "Profile", icon: "file" },
+  { id: 4, label: "Training", icon: "mail" },
+  { id: 5, label: "Contract", icon: "doc" },
+  { id: 6, label: "Signed", icon: "check" },
+  { id: 7, label: "Ready", icon: "building" },
+  { id: 8, label: "End", icon: "flag" },
+];
+
+export const Acesses = [
+  {
+    title: "Email",
+    status: "Active",
+  },
+  {
+    title: "Parsec",
+    status: "Active",
+  },
+  {
+    title: "Frame.i.o",
+    status: "Pending",
+  },
+];
+
+export const tools = [
+  {
+    title: "Epidemic Sound",
+    status: "Not Granted",
+  },
+  {
+    title: "MotionVFX",
+    status: "Not Granted",
+  },
+  {
+    title: "Gling",
+    status: "Not Granted",
+  },
+];
